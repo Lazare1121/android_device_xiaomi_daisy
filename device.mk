dd06010c89a0b50a75ae0bf9f9f87927f547c45e
sbwml <984419930@qq.com>
2021-07-30 17:59:26 +0300

    daisy: Update WFD blobs from LA.UM.9.6.2.r1-03300-89xx.0
    Change-Id: I86ba9ee29727f8a0fde185543d7b4b15c42654c5

--- device.mk
+++ device.mk
@@ -206,10 +206,13 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
+    libdisplayconfig.qti.vendor \
     libqdMetaData.system \
     libgenlock \
     libtinyxml \
-    libvulkan
+    libvulkan \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
